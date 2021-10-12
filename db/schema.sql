44369257a366fb0716745e7169f1e5201e274759
zugzw4ng <bongcloud2001@gmail.com>
2021-10-12 17:17:11 -0400

    updated schema to match assignments

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NULL,
+    name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(100) NULL,
+    title VARCHAR(30) NULL,
     salary DECIMAL(10,3) NULL,
     department_id INT NULL,
     PRIMARY KEY (id)
@@ -18,8 +18,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100) NULL,
-    last_name VARCHAR(100) NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
     role_id INT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id)
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+const connection = require('./config/connection');
 require('dotenv').config();
+
 
 
