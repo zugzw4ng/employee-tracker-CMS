f06bef886687a22dc236601525adb8288042ae98
zugzw4ng <bongcloud2001@gmail.com>
2021-10-12 17:23:45 -0400

    added first action prompt

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,9 @@ const mySql = require("mysql2");
 const connection = mySql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
+    password: process.env.DB_PASS,
+    port: 3001,
+    database: "employees_db"
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -4,5 +4,28 @@ const consoleTable = require('console.table');
 const connection = require('./config/connection');
 require('dotenv').config();
 
+connection.connect(function (err) {
+    if (err) throw err;
+
+    startPrompt();
+});
+
+function startPrompt() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Exit"
+        ] 
+    })
+}
 
 
